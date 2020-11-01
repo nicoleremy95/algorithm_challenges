9f92b1791b60a09108f9f6b0e12e078f6efdbdf6
Nicole Remy <nicoleremy95@gmail.com>
2020-10-31 22:56:44 -0700

    diagonal difference hackerrank challenge

--- index.js
+++ index.js
@@ -10,3 +10,26 @@ function simpleArraySum(ar) {
     return total;
  
 }
+
+
+function diagonalDifference(arr) {
+    // Write your code here
+    const lengthDiagonal = arr.length;
+    let firstDiagonalSum = 0;
+    let secondDiagonalSum = 0;
+    let sum = 0;
+ 
+    for (let i=0; i<lengthDiagonal; i++){
+        firstDiagonalSum += arr[i][i];
+        secondDiagonalSum += arr[lengthDiagonal -1 -i][i]
+    }
+    sum = (firstDiagonalSum-secondDiagonalSum);
+ 
+    if(sum>0) {
+        return sum
+    } else if (sum<0) {
+        return sum * -1
+    } else {
+        return 0
+    }
+}
