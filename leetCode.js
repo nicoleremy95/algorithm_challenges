50c07ab91fed77c5e42bde452977621f351beb84
Nicole Remy <nicoleremy95@gmail.com>
2020-11-18 18:45:04 -0800

    started leetcode algorithm challenge

---
+++ leetCode.js
@@ -0,0 +1,11 @@
+var findTarget = function(root,k){
+    let current;
+    if(root.val < k){
+        var current = root;
+        if(root.val <k){
+            let nextVal = current.right;
+            if(nextVal.val + current.val === k) return true;
+        }
+
+    }
+}
